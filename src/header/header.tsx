944df81f591ce84f26ec436d06300fc0d89d135d
Subhradip <subhradipdas6969@gmail.com>
2024-10-14 10:39:53 +0530

    final fix

--- src/header/header.tsx
+++ src/header/header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header: React.FC = () => {
     return (
         <div className="ms-5 leading-tight whitespace-pre">
-            <pre>
+            <pre className='text-sm'>
                 {/* {`
 ██╗  ██╗███████╗██╗   ██╗     █████╗ ██╗     ██╗███████╗███╗   ██╗
 ██║  ██║██╔════╝╚██╗ ██╔╝    ██╔══██╗██║     ██║██╔════╝████╗  ██║
